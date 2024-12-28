2fdccaa829fdd32527fc27e01ecae4320b906e0e
Pochat2077 <pryadkadima@gmail.com>
2024-12-28 12:30:11 +0200

    lesson 5
    last lesson

--- NetworkFPS/Assets/Scripts/ConnectionsToServer.cs
+++ NetworkFPS/Assets/Scripts/ConnectionsToServer.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using TMPro;
 
 public class ConnectionsToServer : MonoBehaviourPunCallbacks
 {
+  public static ConnectionsToServer Instance { get; private set;}
+  [SerializeField]
+  private TMP_InputField inputRoomName;
+  [SerializeField]
+  private TMP_Text roomName;
    
   private void Awake()
   {
+    Instance = this;
     PhotonNetwork.ConnectUsingSettings();
   }
 
@@ -20,5 +27,25 @@ public class ConnectionsToServer : MonoBehaviourPunCallbacks
     {
         Debug.Log($"Connected to Lobby!");
         UiManager.Instance.OpenPanel("MainMenuPanel");
+    }
+    public override void OnJoinedRoom()
+    {
+      UiManager.Instance.OpenPanel("GameRoomPanel");
+      roomName.text = PhotonNetwork.CurrentRoom.Name;
+    }
+    public void LeaveRoom()
+    {
+      PhotonNetwork.LeaveRoom();
+    }
+    public override void OnLeftRoom()
+    {
+      UiManager.Instance.OpenPanel("MainMenuPanel");
+    }
+    
+    public void CreateNewRoom()
+    {
+      if(string.IsNullOrEmpty(inputRoomName.text)) return;
+
+      PhotonNetwork.CreateRoom(inputRoomName.text);
     }
 }
